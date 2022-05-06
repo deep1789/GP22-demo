f42805b734180081e133c7fe7b33a48c2d9bad61
deep1789 <deepaknewthakur@gmail.com>
2022-05-06 10:41:17 +0530

    added one more test case in string-vowelfinder

--- strings-vowelsFinder.cpp
+++ strings-vowelsFinder.cpp
@@ -13,7 +13,7 @@ int main()
 	while(str[i] != '\0')
 	{
 		
-		if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+		if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
 		{
 			
 			printf("\nvowel = %c found in the string",str[i]);
